bb019375ab41e7063ac18750d21050f29128ed28
Andrei_Sarancha <133157693+Andrei2316@users.noreply.github.com>
2024-04-23 16:40:55 +0300

    update admin panel

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ import asyncio
 
 from aiogram import Bot, Dispatcher, types
 from aiogram.enums import ParseMode
+from aiogram.fsm.strategy import FSMStrategy
 from aiogram.types import BotCommandScopeAllPrivateChats
 from dotenv import find_dotenv, load_dotenv
+from aiogram.client.bot import DefaultBotProperties
+
+from handlers.admin_private import admin_router
 
 load_dotenv(find_dotenv())  # Работа с ключем токеном
 
@@ -12,13 +16,17 @@ from handlers.user_private import user_private_router
 from handlers.user_group import user_group_router
 from common.bot_cmds_list import private
 
-bot = Bot(token=os.getenv('TOKEN'), parse_mode=ParseMode.HTML)  # Экземпляр класса бот
-dp = Dispatcher()  # Экземпляр класса отвечающий за сообщения от сервера телеграмм.
+bot = Bot(token=os.getenv('TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))  # Экземпляр класса бот
+bot.my_admins_list = []
+
+
+
+dp = Dispatcher(fsm_strategy=FSMStrategy.USER_IN_CHAT)  # Экземпляр класса отвечающий за сообщения от сервера телеграмм.
 ALLOWED_UPDATES = ['message', 'edited_message']
 
 dp.include_router(user_private_router)
 dp.include_router(user_group_router)
-
+dp.include_router(admin_router)
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)  # Отлюкаем обработку событий при выключеном боте
--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,71 +1,90 @@
-# Сдесь все обрабоотчики событий
-
-from aiogram import types, Router, F
+from aiogram import F, types, Router
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command, or_f
-from aiogram.types import message
-from aiogram.utils.formatting import as_list, as_marked_section, Bold #Italic, as_numbered_list и тд
+from aiogram.utils.formatting import (
+    as_list,
+    as_marked_section,
+    Bold,
+)  # Italic, as_numbered_list и тд
+
 from filters.chat_types import ChatTypeFilter
-from kbds import reply
+
+from kbds.reply import get_keyboard
 
 user_private_router = Router()
-user_private_router.message.filter(ChatTypeFilter(['private']))
+user_private_router.message.filter(ChatTypeFilter(["private"]))
 
 
 @user_private_router.message(CommandStart())
-async def start_cmd(message: types.Message):  # Функция стартового меню
-    await message.answer('Привет я виртуальный помошник', reply_markup=reply.start_kb3.as_markup(
-                            resize_keyboard=True,
-                            input_field_placeholder='Что Вас интересует?'))  # Стартовое сообщение меню с клавишым меню
+async def start_cmd(message: types.Message):
+    await message.answer(
+        "Привет, я виртуальный помощник",
+        reply_markup=get_keyboard(
+            "Меню",
+            "О магазине",
+            "Варианты оплаты",
+            "Варианты доставки",
+            placeholder="Что вас интересует?",
+            sizes=(2, 2)
+        ),
+    )
 
 
-@user_private_router.message(F.text.lower() == "меню")
+# @user_private_router.message(F.text.lower() == "меню")
 @user_private_router.message(or_f(Command("menu"), (F.text.lower() == "меню")))
-async def menu_cmd(message: types.Message):  # Функция вызов меню
-    await message.answer(f"Вот меню") # reply_markup=reply.del_kb Удаляем или добавляем клавиатуру по необходимости
+async def menu_cmd(message: types.Message):
+    await message.answer("Вот меню:")
 
 
 @user_private_router.message(F.text.lower() == "о магазине")
 @user_private_router.message(Command("about"))
 async def about_cmd(message: types.Message):
-    await message.reply(f"Этот раздел  о компании Pizza&Boost")
+    await message.answer("О нас:")
 
 
 @user_private_router.message(F.text.lower() == "варианты оплаты")
 @user_private_router.message(Command("payment"))
 async def payment_cmd(message: types.Message):
+    text = as_marked_section(
+        Bold("Варианты оплаты:"),
+        "Картой в боте",
+        "При получении карта/кеш",
+        "В заведении",
+        marker="✅ ",
+    )
+    await message.answer(text.as_html())
+
+
+@user_private_router.message(
+    (F.text.lower().contains("доставка")) | (F.text.lower() == "варианты доставки"))
+@user_private_router.message(Command("shipping"))
+async def menu_cmd(message: types.Message):
     text = as_list(
         as_marked_section(
             Bold("Варианты доставки/заказа:"),
             "Курьер",
             "Самовынос (сейчас прибегу заберу)",
             "Покушаю у Вас (сейчас прибегу)",
-            marker='✅ '
+            marker="✅ ",
         ),
         as_marked_section(
             Bold("Нельзя:"),
             "Почта",
             "Голуби",
-            marker='❌ '
+            marker="❌ "
         ),
-        sep='\n----------------------\n'
+        sep="\n----------------------\n",
     )
     await message.answer(text.as_html())
 
 
-@user_private_router.message((F.text.lower().contains("доставк")) | (F.text.lower() == "варианты доставки"))
-@user_private_router.message(Command("shipping"))
-async def shipping_cmd(message: types.Message):
-    await message.answer('<b>Договор об оказании услуг и условия постаки товара.</b>')
+# @user_private_router.message(F.contact)
+# async def get_contact(message: types.Message):
+#     await message.answer(f"номер получен")
+#     await message.answer(str(message.contact))
 
 
-@user_private_router.message(F.contact)
-async def get_contact(message: types.Message):
-    await message.answer(f"номер получен")
-    await message.answer(str(message.contact))
-
-
-@user_private_router.message(F.location)
-async def get_location(message: types.Message):
-    await message.answer(f"локация получена")
-    await message.answer(str(message.location))
+# @user_private_router.message(F.location)
+# async def get_location(message: types.Message):
+#     await message.answer(f"локация получена")
+#     await message.answer(str(message.location))
